9027e0d57bc71b405cda59061edc1401c0ac5a9f
masoo <m.goudarzi8933@gmail.com>
2024-02-06 14:40:09 +0330

    first commit

---
+++ slider/assets/js/slider.js
@@ -0,0 +1,16 @@
+const slides = document.querySelector('.slides');
+const slidesCount = slides.childElementCount;
+const maxLeft = (slidesCount - 1) * 100 * -1;
+
+let current = 0;
+setInterval(function(){
+    if (current > maxLeft) {
+        current -= 100; // current = current - 100
+
+        slides.style.left = current + "%";
+
+    } else {
+        current = 0;
+        slides.style.left = 0;
+    }
+}, 1500);
