def5a66f766a1a49087828fd100824824ef54259
masoo <m.goudarzi8933@gmail.com>
2024-09-01 17:12:35 +0330

    add event listeners

--- Grocery List/script.js
+++ Grocery List/script.js
@@ -7,3 +7,18 @@ const submit = document.querySelector('.addItems-submit');
 const list = document.querySelector('.grocery-list');
 const displayItemsAction = document.querySelector('.displayItems-action');
 const clear = document.querySelector('.displayItems-clear');
+
+
+//Add event listeners
+//Submit listener
+submit.addEventListener('click', addItem);
+
+//Check for local storage
+document.addEventListener('DOMContentLoaded', displayStorage);
+
+//Clear list
+clear.addEventListener('click', removeItems);
+
+//Listen to list to delete individual items
+list.addEventListener('click', removeSingleItem);
+
