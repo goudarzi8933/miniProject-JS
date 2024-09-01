a56ba4de0adbea7c2d050013a9b15f7170b03105
masoo <m.goudarzi8933@gmail.com>
2024-09-01 17:07:43 +0330

    add Item and Display Item

---
+++ Grocery List/script.js
@@ -0,0 +1,9 @@
+//Add items container
+const addItemsAction = document.querySelector('.addItems-action');
+const input = document.querySelector('.addItems-input');
+const submit = document.querySelector('.addItems-submit');
+
+//Display items container
+const list = document.querySelector('.grocery-list');
+const displayItemsAction = document.querySelector('.displayItems-action');
+const clear = document.querySelector('.displayItems-clear');
